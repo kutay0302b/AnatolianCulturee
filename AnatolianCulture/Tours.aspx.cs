99c874e7cd29eaa26e9b080c3da66927c57c9130
kutaytoren <kutaytoren@3eendustriyel.com.tr>
2024-11-05 10:43:08 +0300

    fix

--- AnatolianCulture/Tours.aspx.cs
+++ AnatolianCulture/Tours.aspx.cs
@@ -11,39 +11,53 @@ public partial class _Tours : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if (!IsPostBack)
+        //{
+        //    int tourType = 0;
+        //    int child = 0;
+        //    int nature = 0;
+        //    int cultural = 0;
+        //    int enjoy = 0;
+
+        //    var tours = TourProvider.GetTours();
+        //    if (int.TryParse(Request.QueryString["tourType"], out tourType))
+        //    {
+        //        FilteredTours = tours.FindAll(t => t.TurType == tourType);
+        //    }
+        //    else if (int.TryParse(Request.QueryString["child"], out child))
+        //    {
+        //        FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Çocuk"));
+        //    }
+        //    else if (int.TryParse(Request.QueryString["nature"], out nature))
+        //    {
+        //        FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Doğa"));
+
+        //    }
+        //    else if (int.TryParse(Request.QueryString["cultural"], out cultural))
+        //    {
+        //        FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Kültürel"));
+
+        //    }
+        //    else if (int.TryParse(Request.QueryString["enjoy"], out enjoy))
+        //    {
+        //        FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Eğlence"));
+
+        //    }
+        //}
         if (!IsPostBack)
         {
-            int tourType = 0;
-            int child = 0;
-            int nature = 0;
-            int cultural = 0;
-            int enjoy = 0;
-
             var tours = TourProvider.GetTours();
-            if (int.TryParse(Request.QueryString["tourType"], out tourType))
-            {
-                FilteredTours = tours.FindAll(t => t.TurType == tourType);
-            }
-            else if (int.TryParse(Request.QueryString["child"], out child))
-            {
-                FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Çocuk"));
-            }
-            else if (int.TryParse(Request.QueryString["nature"], out nature))
-            {
-                FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Doğa"));
+            int tourType, child, nature, cultural, enjoy;
 
-            }
-            else if (int.TryParse(Request.QueryString["cultural"], out cultural))
-            {
-                FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Kültürel"));
-
-            }
-            else if (int.TryParse(Request.QueryString["enjoy"], out enjoy))
-            {
-                FilteredTours = tours.FindAll(t => t.TurAciklama.Contains("Eğlence"));
-
-            }
+            FilteredTours = tours.FindAll(t =>
+                (int.TryParse(Request.QueryString["tourType"], out tourType) && t.TurType == tourType) ||
+                (int.TryParse(Request.QueryString["child"], out child) && t.TurAciklama.Contains("Çocuk")) ||
+                (int.TryParse(Request.QueryString["nature"], out nature) && t.TurAciklama.Contains("Doğa")) ||
+                (int.TryParse(Request.QueryString["cultural"], out cultural) && t.TurAciklama.Contains("Kültürel")) ||
+                (int.TryParse(Request.QueryString["enjoy"], out enjoy) && t.TurAciklama.Contains("Eğlence"))
+            );
         }
+
 
     }
     protected string GetPhotoPath(string photoName)
