47f74bbd9109f755d9d705e566ed396518f78130
kutaytoren <kutaytoren@3eendustriyel.com.tr>
2024-11-05 11:01:18 +0300

    roiting ayarlanması

---
+++ AnatolianCulture/App_Code/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Routing;
+using System.Web;
+
+public class RouteConfig
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        // .aspx uzantısını kaldırmak için RouteDefaults ekleme
+        routes.RouteExistingFiles = true;
+
+        // Ana sayfa yönlendirmesi
+        routes.MapPageRoute("HomeRoute", "home", "~/Default.aspx");
+
+        // Tur sayfası yönlendirmesi - Örneğin: /tours
+        routes.MapPageRoute("ToursRoute", "tours", "~/Tours.aspx");
+
+        // Detay sayfası yönlendirmesi - Örneğin: /tour/5
+        routes.MapPageRoute("TourDetailsRoute", "tour/{id}", "~/TourDetails.aspx");
+
+        // Arama sayfası yönlendirmesi - Örneğin: /search?query=turizm
+        routes.MapPageRoute("SearchRoute", "search", "~/Search.aspx");
+
+        // Diğer yönlendirmeler
+        // Örneğin bir 'about' sayfası için:
+        routes.MapPageRoute("AboutRoute", "about", "~/About.aspx");
+
+        // İhtiyaca göre daha fazla rota ekleyin
+    }
+}
