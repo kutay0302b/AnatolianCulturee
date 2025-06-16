731a3f0b61afc3338a7a5a1091da10ef5b5df89c
Kutay <toren.kutay@gmail.com>
2025-06-16 13:43:27 +0300

    *

--- AnatolianCulture/App_Code/AnatolianCulture.Utility/Tools.cs
+++ AnatolianCulture/App_Code/AnatolianCulture.Utility/Tools.cs
@@ -11,9 +11,9 @@ public class Tools
     public static string GetConnStr()
     {
         string connString = "";
-        //connString = "server=mysql02.trwww.com:3306; database=anatol45_anatolianculture; Uid=AcSqlMa;pwd=11511151Sql@;";
-		 connString = "server=mysql02.trwww.com;port=3306;database=anatol45_anatolianculture;Uid=AcSqlMa;pwd=11511151Sql@;";
+        connString = "server=localhost; database=anatolianculture; Uid=root;pwd=12345;";
 
-         return connString;
+
+        return connString;
     }
 }
