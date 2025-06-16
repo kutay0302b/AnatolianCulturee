b86b99600913fbbe8f9769c771cbed547bb35cfe
kutay tören <40161645+kutay0302b@users.noreply.github.com>
2025-06-16 13:31:31 +0300

    Update Tools.cs

--- AnatolianCulture/App_Code/AnatolianCulture.Utility/Tools.cs
+++ AnatolianCulture/App_Code/AnatolianCulture.Utility/Tools.cs
@@ -11,8 +11,7 @@ public class Tools
     public static string GetConnStr()
     {
         string connString = "";
-        //connString = "server=mysql02.trwww.com:3306; database=anatol45_anatolianculture; Uid=AcSqlMa;pwd=11511151Sql@;";
-		 connString = "server=mysql02.trwww.com;port=3306;database=anatol45_anatolianculture;Uid=AcSqlMa;pwd=11511151Sql@;";
+        
 
          return connString;
     }
